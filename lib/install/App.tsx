db7da00ef505630370a846b2a315693f858645bb
lsproule <lucas.sproule.42@gmail.com>
2025-01-15 22:20:58 -0500

    What we've got here is a failure to communicate.
    [Auto-Generated Quote]

---
+++ lib/install/App.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="flex bg-[#242424] gap-12 text-white h-screen flex-col justify-center items-center mx-auto w-screen">
+      <div className="flex text-3xl">
+        <a href="https://guides.rubyonrails.org/index.html" target="_blank">
+          <img
+            src="/images/rails.svg"
+            className="logo rails"
+            alt="Rails logo"
+          />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src="/images/react.svg"
+            className="logo react"
+            alt="React logo"
+          />
+        </a>
+        <a href="https://vite.dev" target="_blank">
+          <img src="/images/vite.svg" className="logo" alt="Vite logo" />
+        </a>
+      </div>
+      <h1 className="text-4xl font-bold">
+        <span className="text-[#CC0000]">Rails </span>#{' '}
+        + <span className="text-[#61dafb]">React </span>
+        + <span className="text-[#646cff]">Vite</span>#{' '}
+      </h1>
+      <div className="card flex flex-col items-center">
+        <button className="mb-4 font-semibold border border-transparent hover:border-[#646cff] cursor-pointer  bg-[#1a1a1a] p-1 rounded-lg p-x-8" onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>app/javascript/components/App.tsx</code> and save to test
+          HMR
+        </p>
+      </div>
+      <p className="text-[#888]">
+        Click on the Rails, Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
--- lib/tasks/install.rake
+++ lib/tasks/install.rake
@@ -3,8 +3,8 @@
 #   # Task goes here
 # end
 #
-namespace :vite_react do 
-  desc "Install the vite react setup" 
+namespace :vite_react do
+  desc "Install the vite react setup"
   task :install do
     system "#{RbConfig.ruby} ./bin/rails app:template LOCATION=#{File.expand_path("../install/vite_react.rb", __dir__)}"
   end
