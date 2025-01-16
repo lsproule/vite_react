891be87cdc8874d2715fce4199aa6c924f3353be
lsproule <lucas.sproule.42@gmail.com>
2025-01-16 10:46:34 -0500

    Life’s simple. You make choices and you don’t look back
    [Auto-Generated Quote]

--- lib/vite_react/railtie.rb
+++ lib/vite_react/railtie.rb
@@ -3,25 +3,27 @@ require "rails/railtie"
 require "rails/generators"
 require "rails/generators/rails/scaffold_controller/scaffold_controller_generator"
 
+# Explicitly require your custom generator file
+require_relative "../generators/vite_react/scaffold_controller/scaffold_controller_generator"
+
 module ViteReact
   class Railtie < ::Rails::Railtie
-    # Ensure the custom generator is loaded
+    # Load the custom generator during Rails initialization
     generators do
       Rails::Generators.configure! Rails.application.config.generators
-      require_relative "../generators/vite_react/scaffold_controller_generator/scaffold_controller_generator"
     end
 
-    # Hook to extend the scaffold_controller behavior
     initializer "vite_react.add_generator_hooks" do
+      # Prepend the custom scaffold generator logic
       Rails::Generators::ScaffoldControllerGenerator.prepend ViteReact::Generators::ScaffoldControllerGenerator
     end
 
+    # Optionally load rake tasks
     rake_tasks do
-      load "tasks/install.rake" 
+      load "tasks/install.rake"
     end
   end
 end
-
 
 #require "rails"
 #require "rails/railtie"
