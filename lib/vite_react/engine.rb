4431e2d84f01e8b72a4d232655fe9a1c5f29b876
lsproule <lucas.sproule.42@gmail.com>
2025-01-17 10:07:42 -0500

    To be or not to be, that is the question.
    [Auto-Generated Quote]

--- lib/vite_react/engine.rb
+++ lib/vite_react/engine.rb
@@ -3,7 +3,6 @@ require "rails"
 module ViteReact
   class Engine < ::Rails::Engine
     config.app_generators do |g|
-      Rails::Generators.configure! app.config.generators
       g.scaffold_controller :vite_react
     end
   end
