21324bcbeb05c9b840ebcd42248f3442d098f77c
lsproule <lucas.sproule.42@gmail.com>
2025-01-17 09:56:12 -0500

    I'm not a smart man, but I know what love is.
    [Auto-Generated Quote]

--- lib/generators/vite_react/scaffold/scaffold_generator.rb
+++ lib/generators/vite_react/scaffold/scaffold_generator.rb
@@ -1,23 +1,35 @@
+require "rails/generators/erb/scaffold/scaffold_generator"
 require "rails/generators/resource_helpers"
-#require "rails/generators/scaffold/scaffold_generator"
 
-# lib/generators/rails/typescript/typescript_generator.rb
-
-module Vite_React
+module ViteReact
   module Generators
-    class ScaffoldGenerator < Rails::Generators::ScaffoldControllerGenerator
+    class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
       include Rails::Generators::ResourceHelpers
 
       source_root File.expand_path("templates", __dir__)
+      source_paths << "lib/templates/erb/scaffold"
 
-      puts "am I being ran"
       argument :attributes, type: :array, default: [], banner: "field:type field:type"
-      hook_for :scaffold, type: :boolean, default: true
+
+      def create_root_folder
+        empty_directory File.join("app/views", controller_file_path)
+      end
+
+      def copy_view_files
+        available_views.each do |view|
+          formats.each do |format|
+            filename = filename_with_extensions(view, format)
+            template filename, File.join("app/views", controller_file_path, filename)
+          end
+        end
+
+        template "partial.html.erb", File.join("app/views", controller_file_path, "_#{singular_name}.html.erb")
+      end
+
 
       def create_or_update_types
         types_file_path = Rails.root.join("app/javascript/types.d.ts")
-
-        interface_code = generate_interface_code
+        interface_code  = generate_interface_code
 
         if File.exist?(types_file_path)
           append_to_file types_file_path, interface_code
@@ -26,16 +38,12 @@ module Vite_React
         end
       end
 
-
-
-      private
-
       def generate_interface_code
         attributes_lines = attributes.flat_map do |attr|
           if attr.type == :references
-            build_reference_lines(attr)
+            build_ts_reference_lines(attr)
           else
-            [ build_attribute_line(attr) ]
+            [ build_ts_attribute_line(attr) ]
           end
         end
 
@@ -46,30 +54,31 @@ module Vite_React
           }
         TS
       end
-
-
-      def build_attribute_line(attr)
-        "#{attr.name}?: #{rails_to_ts_type(attr.type)};"
-      end
-
-      def build_reference_lines(attr)
-        referenced_interface_name = attr.name.camelize
-        [
-          "#{attr.name}_id: number;",
-          "#{attr.name}?: #{referenced_interface_name};"
-        ]
-      end
-
-      def rails_to_ts_type(rails_type)
-        case rails_type
-        when "integer", "float", "decimal" then "number"
-        when "boolean" then "boolean"
-        else
-          "string"
+      private
+        def build_ts_attribute_line(attr)
+          "#{attr.name}?: #{rails_to_ts_type(attr.type)};"
         end
-      end
+
+        def build_ts_reference_lines(attr)
+          referenced_interface_name = attr.name.camelize
+          [
+            "#{attr.name}_id: number;",
+            "#{attr.name}?: #{referenced_interface_name};"
+          ]
+        end
+
+        def rails_to_ts_type(rails_type)
+          case rails_type.to_s
+          when "integer", "float", "decimal" then "number"
+          when "boolean"                     then "boolean"
+          else
+            "string"
+          end
+        end
+        def available_views
+          %w[index edit show new _form]
+        end
     end
   end
 end
-
 
--- lib/vite_react/engine.rb
+++ lib/vite_react/engine.rb
@@ -3,6 +3,7 @@ require "rails"
 module ViteReact
   class Engine < ::Rails::Engine
     config.app_generators do |g|
+      Rails::Generators.configure! app.config.generators
       g.scaffold_controller :vite_react
     end
   end
