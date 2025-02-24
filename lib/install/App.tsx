ecb963320b42d34aee1733e9570e1ce947b24343
lsproule <lucas.sproule.42@gmail.com>
2025-02-24 17:15:05 -0500

    update

--- lib/install/App.tsx
+++ lib/install/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-export function App() {
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
--- lib/install/application.jsx
+++ lib/install/application.jsx
@@ -1,3 +1,4 @@
+import "@hotwired/turbo-rails";
 import React from "react";
 import ReactDOM from "react-dom/client";
 
